ca191e9b865f9526c2b4fbddef753ecbaccc68e6
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2023-05-19 22:01:04 -0300

    build: Installed flutter_riverpod and flutter_provider packages in pubspec.yaml

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,5 +1,6 @@
 import 'package:design_system/design_system.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:simple_music_app/app/app_routes.dart';
 
 class MyApp extends StatelessWidget {
@@ -7,10 +8,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: Themes.light,
-      routes: AppRoutes.all,
+    return ProviderScope(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: Themes.light,
+        routes: AppRoutes.all,
+      ),
     );
   }
 }
